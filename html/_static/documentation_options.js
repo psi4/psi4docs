9a9fb0ff1a7d2f558077dd9717a7aa7a5e8b17f3
GitHub Action <action@github.com>
2022-03-29 15:11:06 +0000

    auto-generated from Psi4 master

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'bf74a61',
+    VERSION: 'aa6b5da',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
